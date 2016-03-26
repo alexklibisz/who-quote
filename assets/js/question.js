199eab9b026a7c5f1db89b7c3e74836bf2bc1d3d
Sam Rose <samrose3@gmail.com>
2016-03-26 19:23:49 -0400

    still workin on freakin ajax shit

--- assets/js/question.js
+++ assets/js/question.js
@@ -1,18 +1,76 @@
 (function() {
 	'use strict';
 
-	var speakers = null;
-	var speakerSelected = false;
+	var speakers = [];
+	var answered = false;
+	var question = {};
 
 	function selectSpeaker() {
 		var el = this;
 
-		for (var i = 0; i < speakers.length; i++) {
-	      speakers[i].classList.remove('selected');
-	    }
-	    el.classList.add('selected');
+		question = document.getElementById('question').dataset.question;
+		var qId = question.id
+		console.log(el.dataset, qId);
 
-	    document.getElementById('q-action').innerHTML = 'Submit';
+		$.ajax({
+		    url: '/question/' + qId,
+		    method: 'POST',
+
+		    // Tell the server we're sending JSON--not strictly necessary with Sails,
+		    // but recommended.
+		    contentType: 'application/json', 
+
+		    // Don't URLencode data
+		    processData: false,
+
+		    // Stringify the data--otherwise it will send "[object object]"
+		    data: JSON.stringify({
+		        selectedSpeaker: el.dataset.th
+		     }),
+
+		    success: function(result) {
+		        // window.location('/character/show/'+$("a#identity-edit").attr("data-id"));
+		        console.log('Lets see... ', result);
+		        displayAns(result.isCorrect);
+		    },
+		    failure: function(msg) {
+		        alert("Fail : " + msg);
+		    },
+		    error: function(xhr, status, text) {
+		        alert(text);
+		        var response = jQuery.parseJSON(xhr, responseText);
+		        alert(response.error);
+		    }
+		});  
+
+		// for (var i = 0; i < speakers.length; i++) {
+	 //      speakers[i].classList.remove('selected');
+	 //    }
+	 //    el.classList.add('selected');
+	    //document.getElementById('q-action').innerHTML = 'Submit';
+	}
+
+	function displayAns(isCorrect) {
+		var answered = true;
+		if (isCorrect) {
+			// Good job! :)
+			console.log(answered, question);
+
+		} else {
+			// Awww.. better luck next time :(
+
+		}
+		$('action').html = 'Next';
+	}
+
+	function nextOrForfiet() {
+		if (answered) {
+			// next
+			window.location('game/' + question.game.id + '/question' + question.id);
+		} else {
+			// home
+			window.location('game/select-category');
+		}
 	}
 
 	function setHandlers() {
@@ -20,6 +78,10 @@
 	    for (var i = 0; i < speakers.length; i++) {
 	      speakers[i].addEventListener('click', selectSpeaker, false);
 	    }
+
+	    $( 'action' ).on( 'click', function(event) {
+		  nextOrForfiet();
+		});
 
 	    console.log('set handlers');
 	}
