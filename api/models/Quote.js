92f296c5be65b16feb82614653d1944ca9d30833
Alex Klibisz <aklibisz@gmail.com>
2016-03-26 11:08:11 -0400

    first pass at models

---
+++ api/models/Quote.js
@@ -0,0 +1,23 @@
+module.exports.attributes = {
+
+  category: {
+    model: 'Quote',
+    required: true
+  },
+
+  speaker: {
+    model: 'Speaker',
+    required: true
+  },
+
+  text: {
+    type: 'string',
+    required: true
+  },
+
+  sourceURL: {
+    type: 'string',
+    required: true
+  }
+
+};
---
+++ api/models/Speaker.js
@@ -0,0 +1,25 @@
+/**
+ * Speaker.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports.attributes = {
+
+  name: {
+    type: 'string',
+    required: true
+  },
+
+  twitterHandle: {
+    type: 'string',
+    required: true
+  },
+
+  category: {
+    model: 'Category',
+    required: true
+  }
+  
+};
