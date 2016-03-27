f1a9925b9d89eb50954511453340dbd030393584
Alex Klibisz <aklibisz@gmail.com>
2016-03-27 03:59:15 -0400

    moving twitter config to env variables

---
+++ config/twitter.js
@@ -0,0 +1,6 @@
+module.exports.twitter = {
+  consumer_key: process.env.WQ_TW_CONSUMER_KEY,
+  consumer_secret: process.env.WQ_TW_CONSUMER_SECRET,
+  access_token_key: process.env.WQ_TW_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.WQ_TW_ACCESS_TOKEN_SECRET
+};
