d88fc276f3f4ce4bb412cced346a6878e4ab285a
Alex Klibisz <aklibisz@gmail.com>
2016-03-30 18:25:58 -0400

    openshift configs

--- config/env/production.js
+++ config/env/production.js
@@ -18,7 +18,7 @@ module.exports = {
 
   port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
-//  host: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+  host: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
 
   log: {
     level: "debug"
